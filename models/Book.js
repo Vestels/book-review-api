1fb95950e27558092325e2056df00a2c9745ffc2
Rokszin Roland <rokszinroland@gmail.com>
2024-10-31 09:19:35 +0100

    Book Schema created

---
+++ models/Book.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: { type: String, required: true },
+  averageRating: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
